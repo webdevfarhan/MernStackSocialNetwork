9ecb166abac874109c8d6d7b4ef7816bc3fae363
Farhan <farhan@nexgin.com>
2019-02-19 22:35:53 +0530

    add error handlers validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 //get User Model
 const User = require('../../models/User');
 
@@ -20,9 +23,17 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works!' }));
 // @access public
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    res.status(400).json({ errors });
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //size
