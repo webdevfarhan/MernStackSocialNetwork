2bdaee4a4bcbd29f30248cfa3ed353452690234e
Farhan <farhan@nexgin.com>
2019-02-20 21:42:54 +0530

    handle login error messages

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,15 +8,10 @@ const passport = require('passport');
 
 //Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 //get User Model
 const User = require('../../models/User');
-
-// @route GET api/users/test
-// @description Tests users routes
-// access public
-
-router.get('/test', (req, res) => res.json({ msg: 'Users works!' }));
 
 // @route GET api/users/register
 // @description Register User
@@ -24,7 +19,7 @@ router.get('/test', (req, res) => res.json({ msg: 'Users works!' }));
 
 router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
-
+  console.log('valid  : ', isValid);
   //check validation
   if (!isValid) {
     res.status(400).json({ errors });
@@ -69,6 +64,13 @@ router.post('/register', (req, res) => {
 // @access public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  console.log('valid  : ', isValid);
+  //check validation
+  if (!isValid) {
+    res.status(400).json({ errors });
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -76,7 +78,8 @@ router.post('/login', (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     //Check Password
@@ -94,7 +97,8 @@ router.post('/login', (req, res) => {
           });
         });
       } else {
-        return res.status(400).json({ password: 'Password Incorrect' });
+        errors.password = 'Password Incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
